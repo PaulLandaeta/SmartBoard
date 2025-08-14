399b868623fa57f2b65ab6527b758b1aa312ee7a
JorgiJust <saenz.jorge.inigues@gmail.com>
2025-08-14 19:10:49 -0400

    Resolve conflicts

--- app/src/main/java/edu/upb/lp/test/activities/MainActivity.java
+++ app/src/main/java/edu/upb/lp/test/activities/MainActivity.java
@@ -1,15 +1,13 @@
 package edu.upb.lp.test.activities;
 
 import io.appium.java_client.AppiumDriver;
-
-
 import java.util.Objects;
-
+import org.openqa.selenium.By;
 
 public class MainActivity {
 
     private final AppiumDriver driver;
-
+    private final By botones = By.id("edu.upb.lp.genericgame:id/buttons");
 
     public MainActivity(AppiumDriver driver) {
         this.driver = driver;
@@ -19,5 +17,25 @@ public class MainActivity {
         return Objects.requireNonNull(driver.getPageSource()).contains("Bug World");
     }
 
+    public boolean isButtonsVisible() {
+        return driver.findElement(botones).isDisplayed();
+    }
 
+    public void clickButtonRestart() {
+        driver.findElement(By.xpath("//android.widget.Button[@text=\"RESTART\"]")).click();
+    }
+
+    public void confirmRestartDialogIfPresent() {
+        if (!driver.findElements(By.id("android:id/content")).isEmpty()) {
+            driver.findElement(By.id("android:id/button1")).click();
+        }
+    }
+
+    public void clickButtonPassDay() {
+        driver.findElement(By.xpath("//android.widget.Button[@text=\"PASS DAY\"]")).click();
+    }
+
+    public void clickButtonBuyFood() {
+        driver.findElement(By.xpath("//android.widget.Button[@text=\"BUY FOOD\"]")).click();
+    }
 }
