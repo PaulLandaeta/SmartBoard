5c0a037ea609d573122a88cc6e14e55cb3ed8bbb
RebecaNavarro <becanavarro2003@gmail.com>
2025-08-14 21:57:04 -0400

    Fix: Session without extra lines and MainActivity path problems

--- app/src/main/java/edu/upb/lp/test/activities/MainActivity.java
+++ app/src/main/java/edu/upb/lp/test/activities/MainActivity.java
@@ -1,41 +1,41 @@
 package edu.upb.lp.test.activities;
 
 import io.appium.java_client.AppiumDriver;
-import java.util.Objects;
+
 import org.openqa.selenium.By;
+
+import java.util.Objects;
 
 public class MainActivity {
 
     private final AppiumDriver driver;
-    private final By botones = By.id("edu.upb.lp.genericgame:id/buttons");
 
     public MainActivity(AppiumDriver driver) {
         this.driver = driver;
     }
 
-    public boolean isHeaderTextVisible() {
-        return Objects.requireNonNull(driver.getPageSource()).contains("Bug World");
-    }
+    private final By botones = By.id("edu.upb.lp.genericgame:id/buttons");
 
     public boolean isButtonsVisible() {
         return driver.findElement(botones).isDisplayed();
     }
 
     public void clickButtonRestart() {
-        driver.findElement(By.xpath("//android.widget.Button[@text="RESTART"]")).click();
+        driver.findElement(By.xpath("//android.widget.Button[@text=\"RESTART\"]")).click();
     }
-
     public void confirmRestartDialogIfPresent() {
         if (!driver.findElements(By.id("android:id/content")).isEmpty()) {
             driver.findElement(By.id("android:id/button1")).click();
         }
     }
-
     public void clickButtonPassDay() {
-        driver.findElement(By.xpath("//android.widget.Button[@text="PASS DAY"]")).click();
+        driver.findElement(By.xpath("//android.widget.Button[@text=\"PASS DAY\"]")).click();
+    }
+    public void clickButtonBuyFood() {
+        driver.findElement(By.xpath("//android.widget.Button[@text=\"BUY FOOD\"]")).click();
     }
 
-    public void clickButtonBuyFood() {
-        driver.findElement(By.xpath("//android.widget.Button[@text="BUY FOOD"]")).click();
+    public boolean isHeaderTextVisible() {
+        return Objects.requireNonNull(driver.getPageSource()).contains("Bug World");
     }
 }
--- app/src/main/java/test/session/Session.java
+++ app/src/main/java/test/session/Session.java
@@ -1,44 +1,30 @@
 package test.session;
 
-import android.os.Build;
-import androidx.annotation.NonNull;
-import io.appium.java_client.AppiumDriver;
+
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.AppiumDriver;
+import test.factoryDevices.FactoryDevices;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.Duration;
-
 public class Session {
     private static Session session = null;
-    private final AppiumDriver device;
-
+    private AppiumDriver device;
     private Session() {
-        DesiredCapabilities caps = getDesiredCapabilities();
-
-        try {
-            device = new AndroidDriver(
-                    new URL("http://127.0.0.1:4723/"), caps);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                device.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            }
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("URL de Appium malformada", e);
-        }
-    }
-
-    @NonNull
-    private static DesiredCapabilities getDesiredCapabilities() {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "Android");
-        caps.setCapability("appium:deviceName", "Pixel 9 Pro XL");
-        caps.setCapability("appium:platformVersion", "16");
-        caps.setCapability("appium:automationName", "UiAutomator2");
+        caps.setCapability("appium:deviceName", "Practica Interna");
+        caps.setCapability("appium:platformVersion", "9");
+        caps.setCapability("appium:automationName", "uiautomator2");
         caps.setCapability("appium:appPackage", "edu.upb.lp.genericgame");
         caps.setCapability("appium:appActivity", "edu.upb.lp.core.activities.AndroidGameActivity");
-        caps.setCapability("appium:noReset", true);
-        return caps;
+
+        try {
+            device = new AppiumDriver(new URL("http://127.0.0.1:4723/"), caps);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static Session getInstance() {
@@ -48,8 +34,9 @@ public class Session {
         return session;
     }
 
-    public AppiumDriver getDevice() {
-        return device;
+    public void closeApp(){
+        device.quit();
+        session = null;
     }
 
     public static void resetInstance() {
@@ -57,5 +44,8 @@ public class Session {
             session.device.quit();
             session = null;
         }
+    }
+    public AppiumDriver getDevice(){
+        return  device;
     }
 }
